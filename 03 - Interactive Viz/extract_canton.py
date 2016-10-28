7d0078143feb342b596dd9d8e76bae510070b330
Sylvain Beaud <sylvain.beaud@epfl.ch>
2016-10-28 12:14:43 +0200

    File updated

--- 03 - Interactive Viz/extract_canton.py
+++ 03 - Interactive Viz/extract_canton.py
@@ -13,7 +13,7 @@ username = 'cataphile22'
 def extractCanton(dataframe, q, raw = None):
     """Try to find the canton of the string given in arg (typically a university name). Beware this function is performing a request to geonames.
     The parameter dataframe is the dataframe created from the cvs file ExtractGrant"""
-	if raw == None: # if raw is not set, it is equal to q
+    if raw == None: # if raw is not set, it is equal to q
         raw = q
     param = {'q': q, 'country': 'CH', 'type': 'json'  ,'username': username}
     
